11c6d9ad954dbed424d432587eca61d7a7627ebc
ScottR <scottadamr01@gmail.com>
2022-09-29 21:56:26 -0400

    add email to contact

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import { Box, Container,Grid, Tooltip, IconButton } from "@mui/material";
+import { Box, Container, Tooltip, IconButton } from "@mui/material";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn'
+import EmailIcon from '@mui/icons-material/Email';
 // import './Contact.css'
 
 const Contact = () => {
   return (
     <Container className='contact' maxWidth={false} style={{background:'#272625'}}>
-      <Box sx={{ height: '100vh'}} className='contact-page'>
-        <Box borderBottom={1} pt={3}>Contact</Box>
+      <Box textAlign="center" pt={{ xs: 3, sm: 1}} pb={{ xs: 3, sm: 0 }}>
+        <Box borderTop={1} pt={3}>Contact</Box>
         <Tooltip title="Github">
         <IconButton href='https://github.com/scotty-cloud'>
           <GitHubIcon />
@@ -17,6 +18,11 @@ const Contact = () => {
       <Tooltip title="LinkedIn">
         <IconButton href='https://www.linkedin.com/in/scottyr/'>
           <LinkedInIcon />
+        </IconButton>
+      </Tooltip>
+      <Tooltip title="Email">
+        <IconButton href='mailto: scottadamr01@gmail.com'>
+          <EmailIcon />
         </IconButton>
       </Tooltip>
       </Box>
