f27dc63c1c478a73bfd57733005cdfdbe87e9333
scotty <scottadamr01@gmail.com>
2023-07-13 22:25:50 -0400

    set placeholder

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -4,17 +4,8 @@ import { Container, Grid, Box, Paper, Typography } from "@mui/material";
 
 const About = () => {
   return (
-    <div
-    className="me"
-    >
-      <p>hello im scotty and i like to design
-        <br/>
-          I like to DIY, Read Books on poetry
-          <br/>
-          I always had a passion for creating and building
-          <br/>
-          Which led me to become a software engineer.
-      </p>
+    <div className="me">
+     
     </div>
     
   )
